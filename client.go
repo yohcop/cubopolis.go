446c39ca9742de62f5d9341cb29225d7bf903399
Yohann R. Coppel <yc@leppoc.net>
2011-03-19 15:03:12 -0700

    Err, Moving around a bit better. Rendering cubes with borders, but perf extremely bad. Around 5fps on my laptop. I must be doing something wrong

--- client.go
+++ client.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "flag"
-    "os"
-    "fmt"
-    "http"
-    "log"
-    "io/ioutil"
-    "json"
-    )
+	"flag"
+	"os"
+	"fmt"
+	"http"
+	"log"
+	"io/ioutil"
+	"json"
+)
 
 var ip = flag.String("address", "cube.leppoc.net", "cuboboplis http server")
 var port = flag.String("port", "10234", "http service port")
@@ -16,38 +16,37 @@ var port = flag.String("port", "10234", "http service port")
 type ChunkData [16][16][]string
 
 type ChunkInfo struct {
-  X int
-  Y int
-  Data *ChunkData
+	X    int
+	Y    int
+	Data *ChunkData
 }
 
 func GetChunk(chunkX, chunkY int) (*ChunkInfo, os.Error) {
-  address := fmt.Sprintf("http://%s:%s/a/r?cy=%d&cx=%d", *ip, *port, chunkY, chunkX)
-  fmt.Println("Making request " + address)
-  r, _, err := http.Get(address)
-  if err != nil {
-    log.Panic(err)
-  }
-  defer r.Body.Close()
-  content, err := ioutil.ReadAll(r.Body)
-  if err != nil {
-    log.Panic(err)
-  }
-  info := &ChunkInfo{chunkX, chunkY, &ChunkData{}}
-  err = json.Unmarshal(content, &info.Data)
-  if err != nil {
-    fmt.Println(err.String())
-  }
-  for y := 0; y < 16; y++ {
-    for x := 0; x < 16; x++ {
-      if len(info.Data[y][x]) == 0 {
-        info.Data[y][x] = append(info.Data[y][x], "0")
-      } else if info.Data[y][x][0] == "" {
-        info.Data[y][x][0] = "0"
-      }
-    }
-  }
-  fmt.Println(info.Data)
-  return info, err
+	address := fmt.Sprintf("http://%s:%s/a/r?cy=%d&cx=%d", *ip, *port, chunkY, chunkX)
+	fmt.Println("Making request " + address)
+	r, _, err := http.Get(address)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer r.Body.Close()
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+	info := &ChunkInfo{chunkX, chunkY, &ChunkData{}}
+	err = json.Unmarshal(content, &info.Data)
+	if err != nil {
+		fmt.Println(err.String())
+	}
+	for y := 0; y < 16; y++ {
+		for x := 0; x < 16; x++ {
+			if len(info.Data[y][x]) == 0 {
+				info.Data[y][x] = append(info.Data[y][x], "0")
+			} else if info.Data[y][x][0] == "" {
+				info.Data[y][x][0] = "0"
+			}
+		}
+	}
+	fmt.Println(info.Data)
+	return info, err
 }
-
--- main.go
+++ main.go
@@ -3,86 +3,137 @@ package main
 import "sdl"
 import "gl"
 import "flag"
+import (
+	"math"
+)
+const (
+  PiOver100 float64 = 0.0174532925199433
+  cubeWidth = 0.5
+)
 
 var printInfo = flag.Bool("info", false, "print GL implementation information")
 
 var T0 uint32 = 0
 var Frames uint32 = 0
 
-const cubeWidth = 0.5
 
-func cube(cube string) {
-  gl.Begin(gl.QUADS)
-  // TOP
-  gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
-  gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
-  gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
-  gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
-  // Bottom
-  gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
-  gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
-  gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
-  gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
-  // Front
-  gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
-  gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
-  gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
-  gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
-  // Back
-  gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
-  gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
-  gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
-  gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
-  // Left
-  gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
-  gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
-  gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
-  gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
-  // Right
-  gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
-  gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
-  gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
-  gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
-  gl.End()
+func cube(color []float32) {
+  gl.Color4f(color[0], color[1], color[2], color[3])
+	gl.Begin(gl.QUADS)
+	// TOP
+	gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
+	// Bottom
+	gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
+	// Front
+	gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
+	// Back
+	gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
+	// Left
+	gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
+	// Right
+	gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
+	gl.End()
+
+  gl.Color4f(color[0] / 1.2, color[1] / 1.2, color[2] / 1.2, 1.0)
+  gl.Begin(gl.LINE_STRIP);						// Start Drawing Our Player Using Lines
+	// TOP
+	gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
+	// Front
+	gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
+	// Bottom
+	gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
+	// Back
+	gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(cubeWidth, cubeWidth, -cubeWidth)
+	// Right
+	gl.Vertex3f(cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, cubeWidth)
+	gl.Vertex3f(cubeWidth, -cubeWidth, -cubeWidth)
+	// Left
+	gl.Vertex3f(-cubeWidth, cubeWidth, cubeWidth)
+	gl.Vertex3f(-cubeWidth, cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, -cubeWidth)
+	gl.Vertex3f(-cubeWidth, -cubeWidth, cubeWidth)
+  gl.End();
+  gl.Color4f(1.0, 1.0, 1.0, 1.0)
 }
 
-var view_rotx float64 = 0.0
-var view_roty float64 = 0.0
-var view_rotz float64 = 0.0
-var view_x float64 = 0.0
-var view_y float64 = 0.0
-var view_z float64 = 0.0
+var (
+	yrot                    float32 // camera rotation
+	xpos, zpos              float32 // camera position
+	walkbias, walkbiasangle float32 // head-bobbing....
+	lookupdown              float32
+
+	lightAmbient  = []float32{0.5, 0.5, 0.5, 1.0}
+	lightDiffuse  = []float32{1.0, 1.0, 1.0, 1.0}
+	lightPosition = []float32{0.0, 0.0, 2.0, 1.0}
+)
 
 var cubes map[string]uint = make(map[string]uint)
 var chunks []*ChunkInfo
 
 func draw() {
+  xtrans   := -xpos
+  ztrans   := -zpos
+  ytrans   := -walkbias - 0.25
+  scenroty := 360.0 - yrot
 
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+  // Clear the screen and depth buffer
+  gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-	gl.PushMatrix()
-	gl.Rotated(view_rotx, 1.0, 0.0, 0.0)
-	gl.Rotated(view_roty, 0.0, 1.0, 0.0)
-	gl.Rotated(view_rotz, 0.0, 0.0, 1.0)
-  gl.Translated(view_x, view_z, view_y)
+  // reset the view
+  gl.LoadIdentity()
 
-  for _, chunk := range chunks {
-    for y := 0; y < 16; y++ {
-      for x := 0; x < 16; x++ {
-        for z := 0; z <= 16; z++ {
-          if len(chunk.Data[y][x]) > z && chunk.Data[y][x][z] != "" {
-            gl.PushMatrix()
-            gl.Translated(
-                float64(16 * chunk.X + x),
-                float64(16 * chunk.Y + y),
-                float64(z))
-            gl.CallList(cubes[chunk.Data[y][x][z]])
-            gl.PopMatrix()
-          }
-        }
-      }
-    }
-  }
+  // Rotate up and down to look up and down
+  gl.Rotatef(lookupdown, 1.0, 0.0, 0.0)
+  // Rotate depending on direction player is facing
+  gl.Rotatef(scenroty, 0.0, 1.0, 0.0)
+  // translate the scene based on player position
+  gl.Translatef(xtrans, ytrans - 1.5, ztrans)
+
+	for _, chunk := range chunks {
+		for y := 0; y < 16; y++ {
+			for x := 0; x < 16; x++ {
+				for z := 0; z <= 16; z++ {
+					if len(chunk.Data[y][x]) > z && chunk.Data[y][x][z] != "" {
+						gl.PushMatrix()
+						gl.Translated(
+							float64(16*chunk.X+x),
+							float64(z),
+							float64(16*chunk.Y+y))
+						gl.CallList(cubes[chunk.Data[y][x][z]])
+						gl.PopMatrix()
+					}
+				}
+			}
+		}
+	}
 
 	gl.PopMatrix()
 
@@ -106,62 +157,96 @@ func idle() {
 }
 
 /* new window size or exposure */
+
 func reshape(width int, height int) {
+	// protect against a divide by zero
+	if height == 0 {
+		height = 1
+	}
 
-	h := float64(height) / float64(width)
-
+	// Setup our viewport
 	gl.Viewport(0, 0, width, height)
+
+	// change to the projection matrix and set our viewing volume.
 	gl.MatrixMode(gl.PROJECTION)
 	gl.LoadIdentity()
-	gl.Frustum(-1.0, 1.0, -h, h, 5.0, 1000.0)
+
+	// aspect ratio
+	aspect := float64(width) / float64(height)
+
+	// Set our perspective.
+	// This code is equivalent to using gluPerspective as in the original tutorial.
+	var fov, near, far float64
+	fov = 45.0
+	near = 0.1
+	far = 100.0
+	top := math.Tan(float64(fov*math.Pi/360.0)) * near
+	bottom := -top
+	left := aspect * bottom
+	right := aspect * top
+	gl.Frustum(left, right, bottom, top, near, far)
+
+	// Make sure we're changing the model view and not the projection
 	gl.MatrixMode(gl.MODELVIEW)
+
+	// Reset the view
 	gl.LoadIdentity()
-	gl.Translatef(0.0, 0.0, -40.0)
 }
 
 func fetchChunks(chunkY, chunkX int) {
-  for y := - chunkY; y < chunkY; y++ {
-    for x := - chunkX; x < chunkX; x++ {
-      if chunk, err := GetChunk(y, x); err == nil {
-        chunks = append(chunks, chunk)
-      }
-    }
-  }
+	for y := -chunkY; y < chunkY; y++ {
+		for x := -chunkX; x < chunkX; x++ {
+			if chunk, err := GetChunk(y, x); err == nil {
+				chunks = append(chunks, chunk)
+			}
+		}
+	}
 }
 
 func init_() {
-	pos := []float32{5.0, 5.0, 10.0, 0.0}
+	//pos := []float64{5.0, 5.0, 10.0, 0.0}
 
-  colors := map[string][]float32 {
-	  "0": []float32{0.7, 0.7, 0.7, 1.0},
-	  "1": []float32{1.0, 0.0, 0.0, 1.0},
-    "2": []float32{1,0.66,0,1},
-    "3": []float32{1,1,0,1},
-    "4": []float32{0,1,0,1},
-    "5": []float32{0,0,1,1},
-    "6": []float32{0,0.66,1,1},
-    "7": []float32{1,0,1,1},
-    "8": []float32{1,1,1,1},
-    "9": []float32{0.3,0.3,0.3,1},
-  }
+	colors := map[string][]float32{
+		"0": []float32{0.7, 0.7, 0.7, 1.0},
+		"1": []float32{1.0, 0.0, 0.0, 1.0},
+		"2": []float32{1.0, 0.6, 0.0, 1.0},
+		"3": []float32{1.0, 1.0, 0.0, 1.0},
+		"4": []float32{0.0, 1.0, 0.0, 1.0},
+		"5": []float32{0.0, 0.0, 1.0, 1.0},
+		"6": []float32{0.0, 0.6, 1.0, 1.0},
+		"7": []float32{1.0, 0.0, 1.0, 1.0},
+		"8": []float32{1.0, 1.0, 1.0, 1.0},
+		"9": []float32{0.3, 0.3, 0.3, 1.0},
+	}
 
-  fetchChunks(2, 2)
-	gl.Lightfv(gl.LIGHT0, gl.POSITION, pos)
-	gl.Enable(gl.CULL_FACE)
-	gl.Enable(gl.LIGHTING)
-	gl.Enable(gl.LIGHT0)
-	gl.Enable(gl.DEPTH_TEST)
+	fetchChunks(2, 2)
 
-  for name, color := range colors {
-    /* make a cube */
-    cubes[name] = gl.GenLists(1)
-    gl.NewList(cubes[name], gl.COMPILE)
-    gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, color)
-    cube("")
-    gl.EndList()
-  }
+  //gl.ShadeModel(gl.SMOOTH)
+  gl.ClearColor(0.2, 0.2, 0.6, 0.0)
+  gl.ClearDepth(1.0)
+  gl.Enable(gl.DEPTH_TEST)
+  //gl.DepthFunc(gl.LEQUAL)
+  gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
 
-	gl.Enable(gl.NORMALIZE)
+  gl.Lightfv(gl.LIGHT1, gl.AMBIENT,  lightAmbient )
+  gl.Lightfv(gl.LIGHT1, gl.DIFFUSE,  lightDiffuse )
+  gl.Lightfv(gl.LIGHT1, gl.POSITION, lightPosition)
+  gl.Enable(gl.LIGHT1)
+
+  gl.BlendFunc(gl.SRC_ALPHA, gl.ONE)
+  gl.Hint(gl.LINE_SMOOTH_HINT, gl.NICEST);
+
+  //gl.Enable(gl.LIGHTING)
+  //gl.Enable(gl.BLEND)
+
+	for name, color := range colors {
+		/* make a cube */
+		cubes[name] = gl.GenLists(1)
+		gl.NewList(cubes[name], gl.COMPILE)
+		//gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, color)
+		cube(color)
+		gl.EndList()
+	}
 
 	if *printInfo {
 		print("GL_RENDERER   = ", gl.GetString(gl.RENDERER), "\n")
@@ -170,6 +255,41 @@ func init_() {
 		print("GL_EXTENSIONS = ", gl.GetString(gl.EXTENSIONS), "\n")
 	}
 
+}
+
+// handle key press events
+func handleKeyPress(keysym []uint8) {
+  keys := sdl.GetKeyState()
+
+  if keys[sdl.K_RIGHT] == 1 {
+    yrot -= 3.5
+  }
+
+  if keys[sdl.K_LEFT] == 1 {
+    yrot += 3.5
+  }
+
+  if keys[sdl.K_UP] == 1 {
+    xpos -= float32(math.Sin(float64(yrot) * PiOver100)) * 0.5
+    zpos -= float32(math.Cos(float64(yrot) * PiOver100)) * 0.5
+    if walkbiasangle >= 359.0 {
+      walkbiasangle = 0.0
+    } else {
+      walkbiasangle += 10.0
+    }
+    walkbias = float32(math.Sin(float64(walkbiasangle) * PiOver100)) / 20.0
+  }
+
+  if keys[sdl.K_DOWN] == 1 {
+    xpos += float32(math.Sin(float64(yrot) * PiOver100)) * 0.5
+    zpos += float32(math.Cos(float64(yrot) * PiOver100)) * 0.5
+    if walkbiasangle <= 1.0 {
+      walkbiasangle = 359.0
+    } else {
+      walkbiasangle -= 10.0
+    }
+    walkbias = float32(math.Sin(float64(walkbiasangle) * PiOver100)) / 20.0
+  }
 }
 
 func main() {
@@ -181,7 +301,7 @@ func main() {
 
 	sdl.Init(sdl.INIT_VIDEO)
 
-	var screen = sdl.SetVideoMode(480, 390, 16, sdl.OPENGL|sdl.RESIZABLE)
+	var screen = sdl.SetVideoMode(640, 480, 16, sdl.OPENGL|sdl.RESIZABLE)
 
 	if screen == nil {
 		sdl.Quit()
@@ -216,33 +336,10 @@ func main() {
 		}
 		keys = sdl.GetKeyState()
 
+    handleKeyPress(keys)
 		if keys[sdl.K_ESCAPE] != 0 {
 			done = true
 		}
-		if keys[sdl.K_UP] != 0 {
-			view_rotx += 5.0
-		}
-		if keys[sdl.K_DOWN] != 0 {
-			view_rotx -= 5.0
-		}
-		if keys[sdl.K_LEFT] != 0 {
-			view_rotz += 5.0
-		}
-		if keys[sdl.K_RIGHT] != 0 {
-			view_rotz -= 5.0
-		}
-		if keys[sdl.K_w] != 0 {
-      view_z -= 1.0
-    }
-		if keys[sdl.K_a] != 0 {
-      view_x += 1.0
-    }
-		if keys[sdl.K_s] != 0 {
-      view_z += 1.0
-    }
-		if keys[sdl.K_d] != 0 {
-      view_x -= 1.0
-    }
 
 		draw()
 	}
